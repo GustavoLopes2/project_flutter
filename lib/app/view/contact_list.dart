ee884e261384d3de95842b5c0f86ac395946e6f9
Gustavo Lopes Feitosa <103670918+GustavoLopes2@users.noreply.github.com>
2024-06-23 22:10:42 -0300

    Aula 14 - Adicionando campos estáticos na lista de contatos com imagens e botões sem funcionalidades em cada contato da lista.

--- lib/app/view/contact_list.dart
+++ lib/app/view/contact_list.dart
@@ -2,6 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:projeto_flutter/app/my_app.dart';
 
 class ContactList extends StatelessWidget {
+  final lista = [
+    {
+      'nome': 'Pietro',
+      'telefone': '(11) 9 9874-5656',
+      'avatar':
+          'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png'
+    },
+    {
+      'nome': 'Maite',
+      'telefone': '(11) 9 9214-8578',
+      'avatar':
+          'https://cdn.pixabay.com/photo/2021/11/12/03/04/woman-6787784_1280.png'
+    },
+    {
+      'nome': 'Hortencia',
+      'telefone': '(11) 9 9421-3356',
+      'avatar':
+          'https://cdn.pixabay.com/photo/2017/01/31/13/46/girl-2024190_1280.png'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +36,29 @@ class ContactList extends StatelessWidget {
               icon: Icon(Icons.add))
         ],
       ),
-      body: ListView(),
+      body: ListView.builder(
+        itemCount: lista.length,
+        itemBuilder: (context, i) {
+          dynamic contato = lista[i];
+          var avatar = CircleAvatar(
+            backgroundImage: NetworkImage(contato['avatar']),
+          );
+          return ListTile(
+            leading: avatar,
+            title: Text(contato['nome'].toString()),
+            subtitle: Text(contato['telefone'].toString()),
+            trailing: Container(
+              width: 100,
+              child: Row(
+                children: [
+                  IconButton(onPressed: null, icon: Icon(Icons.edit)),
+                  IconButton(onPressed: null, icon: Icon(Icons.delete))
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
